324a1016cc17e1e283464c67f84caebf6242fc2d
Tretyakov Leonid <treonid@gmail.com>
2023-12-08 20:06:57 +0300

    Update problem rbs

--- sem-4/problem_rbs/main.c
+++ sem-4/problem_rbs/main.c
@@ -1,13 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node_t {
   struct node_t *next;
   int data;
-  int count;
-  int count2;
 };
 
-int main() {
-  int max = 670;
-  printf("%d\n", max / 8 + 1);
+struct bucket {
+  struct node_t *list;
+  int min, max;
+};
+
+int get_max(int *inp, int count) {
+  int max = 0;
+  for (int i = 0; i < count; i++) 
+    if (inp[i] > max)
+      max = inp[i];
+  return max;
 }
+
+void make_bucket(int *inp, int count) {
+  int max = get_max(inp, count);
+  int bucket_size = max / count;
+  struct bucket buckets[count];
+  int add_left;
+
+  if (bucket_size < 1) {
+    fprintf(stderr, "Bucket less then 1\n");
+    abort();
+  }
+
+  for(int i = 0; i < count; i++) {
+    add_left = (i == 0) ? 0: 1;
+    buckets[i].min = i * bucket_size + add_left;
+    buckets[i].max = (i == count - 1) ? max : (i + 1) * bucket_size;
+#if 0
+    printf("Bucket %d Min: %d Max: %d\n", i + 1, buckets[i].min, buckets[i].max);
+#endif
+  }
+  
+#if 0
+  printf("Max is %d\nBucket size is %d\n", max, bucket_size);
+#endif
+}
+
+int main() {
+  int count, res;
+  int *inp; 
+  res = scanf("%d", &count);
+  if (res != 1) {
+    fprintf(stderr, "Input error\n");
+    return 1;
+  }
+
+  inp = (int *)calloc(count, sizeof(int));
+  for(int i = 0; i < count; i++) {
+    res = scanf("%d", &inp[i]);
+    if (res != 1) {
+      fprintf(stderr, "Input error\n");
+      free(inp);
+      return 1;
+    }
+  }
+  make_bucket(inp, count);
+
+  free(inp);
+}
+
