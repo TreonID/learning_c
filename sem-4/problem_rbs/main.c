4d25fc6c6a0df19475343c0cf0aa7a7425fbe895
Tretyakov Leonid <treonid@gmail.com>
2023-12-10 18:55:36 +0300

    Update problem rbs sem-4

--- sem-4/problem_rbs/main.c
+++ sem-4/problem_rbs/main.c
@@ -6,7 +6,7 @@ struct node_t {
   int data;
 };
 
-struct bucket {
+struct bucket_t {
   struct node_t *list;
   int min, max;
 };
@@ -19,29 +19,69 @@ int get_max(int *inp, int count) {
   return max;
 }
 
+void print_buckets(struct bucket_t *buckets, int count) {
+  struct node_t *node;
+  for (int i = 0; i < count; i++) {
+    if (buckets[i].list) {
+      node = buckets[i].list;
+      for (; ; ) {
+        printf("%d ", node->data);
+        if (!(node->next))
+          break;
+        node = node->next;
+      }
+      printf("0 ");
+
+    } else {
+      printf("0 ");
+    }
+    
+  }
+  printf("\n");
+}
+
+void add_to_bucket(struct bucket_t *bucket, int inp) {
+  struct node_t *element;
+
+  element = (struct node_t *)calloc(1, sizeof(struct node_t));
+  element->data = inp;
+  
+  if (bucket->list) {
+
+  } else {
+    bucket->list = element;
+  }
+}
+
 void make_bucket(int *inp, int count) {
   int max = get_max(inp, count);
   int bucket_size = max / count;
-  struct bucket buckets[count];
+  struct bucket_t *buckets;
   int add_left;
 
   if (bucket_size < 1) {
     fprintf(stderr, "Bucket less then 1\n");
     abort();
   }
+  
+  buckets = (struct bucket_t *)calloc(count, sizeof(struct bucket_t));
 
-  for(int i = 0; i < count; i++) {
+  for (int i = 0; i < count; i++) {
     add_left = (i == 0) ? 0: 1;
     buckets[i].min = i * bucket_size + add_left;
     buckets[i].max = (i == count - 1) ? max : (i + 1) * bucket_size;
-#if 0
-    printf("Bucket %d Min: %d Max: %d\n", i + 1, buckets[i].min, buckets[i].max);
-#endif
   }
-  
-#if 0
-  printf("Max is %d\nBucket size is %d\n", max, bucket_size);
-#endif
+
+  for (int i = 0; i < count; i++) {
+    for (int bi = 0; bi < count; bi++) {
+      if (buckets[bi].min <= inp[i] && inp[i] <= buckets[bi].max) {
+        add_to_bucket(&buckets[bi], inp[i]);
+        break;
+      }
+    }
+  }
+ 
+  print_buckets(buckets, count);
 }
 
 int main() {
@@ -62,8 +102,8 @@ int main() {
       return 1;
     }
   }
+
   make_bucket(inp, count);
-
   free(inp);
-}
 
+}
