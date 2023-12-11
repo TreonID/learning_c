aa56b545d91e39a1eb430b007241678317c3eeb0
Tretyakov Leonid <treonid@gmail.com>
2023-12-11 12:19:18 +0300

    update problem rbs sem-4 test for bad interval

--- sem-4/problem_rbs/main.c
+++ sem-4/problem_rbs/main.c
@@ -13,10 +13,51 @@ struct bucket_t {
 
 int get_max(int *inp, int count) {
   int max = 0;
-  for (int i = 0; i < count; i++) 
+  for (int i = 0; i < count; i++)
     if (inp[i] > max)
       max = inp[i];
   return max;
+}
+
+struct node_t *list_insert(struct node_t *top, struct node_t *element) {
+  struct node_t *node;
+  struct node_t *prev;
+
+  if (top) {
+
+    if (top->next) {
+      if (top->data >= element->data) {
+        element->next = top;
+        return element;
+      }
+
+      prev = top;
+      node = top->next;
+      for (; ; ) {
+        if (node->data >= element->data) {
+          prev->next = element;
+          element->next = node;
+          return top;
+        }
+        if (!node->next) {
+          node->next = element;
+          return top;
+        }
+        node = node->next;
+        prev = prev->next;
+      }
+    }
+    
+    if (top->data >= element->data) {
+      element->next = top;
+      return element;
+    } else {
+      top->next = element;
+      return top;
+    }
+
+  }
+  return element;
 }
 
 void print_buckets(struct bucket_t *buckets, int count) {
@@ -35,7 +76,6 @@ void print_buckets(struct bucket_t *buckets, int count) {
     } else {
       printf("0 ");
     }
-    
   }
   printf("\n");
 }
@@ -45,12 +85,8 @@ void add_to_bucket(struct bucket_t *bucket, int inp) {
 
   element = (struct node_t *)calloc(1, sizeof(struct node_t));
   element->data = inp;
-  
-  if (bucket->list) {
 
-  } else {
-    bucket->list = element;
-  }
+  bucket->list = list_insert(bucket->list, element);
 }
 
 void make_bucket(int *inp, int count) {
@@ -59,17 +95,28 @@ void make_bucket(int *inp, int count) {
   struct bucket_t *buckets;
   int add_left;
 
+  printf("Bucket size: %d\n", bucket_size);
+  
   if (bucket_size < 1) {
     fprintf(stderr, "Bucket less then 1\n");
     abort();
   }
-  
-  buckets = (struct bucket_t *)calloc(count, sizeof(struct bucket_t));
 
+  buckets = (struct bucket_t *)calloc(count, sizeof(struct bucket_t));
+#if 0
   for (int i = 0; i < count; i++) {
-    add_left = (i == 0) ? 0: 1;
+    add_left = (i == 0) ? 0 : 1;
     buckets[i].min = i * bucket_size + add_left;
     buckets[i].max = (i == count - 1) ? max : (i + 1) * bucket_size;
+    printf("Bucket %d Min: %d Max: %d\n", i, buckets[i].min, buckets[i].max);
+  }
+#endif
+  
+  for (int i = 0; i < count; i++) {
+    
+    buckets[i].min = i * bucket_size + 1;
+    buckets[i].max = (i + 1) * bucket_size;
+    printf("Bucket %d Min: %d Max: %d\n", i, buckets[i].min, buckets[i].max);
   }
 
   for (int i = 0; i < count; i++) {
@@ -80,13 +127,13 @@ void make_bucket(int *inp, int count) {
       }
     }
   }
- 
+
   print_buckets(buckets, count);
 }
 
 int main() {
   int count, res;
-  int *inp; 
+  int *inp;
   res = scanf("%d", &count);
   if (res != 1) {
     fprintf(stderr, "Input error\n");
@@ -94,7 +141,7 @@ int main() {
   }
 
   inp = (int *)calloc(count, sizeof(int));
-  for(int i = 0; i < count; i++) {
+  for (int i = 0; i < count; i++) {
     res = scanf("%d", &inp[i]);
     if (res != 1) {
       fprintf(stderr, "Input error\n");
@@ -105,5 +152,5 @@ int main() {
 
   make_bucket(inp, count);
   free(inp);
-
 }
+
