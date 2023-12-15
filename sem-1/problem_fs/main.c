53a8ded3c122b751638eaf447e17c18e56b52072
Tretyakov Leonid <treonid@gmail.com>
2023-12-15 11:45:35 +0300

    Complete problem fs sem-1

---
+++ sem-1/problem_fs/main.c
@@ -0,0 +1,60 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned fact(unsigned x) {
+  if (x == 0) return 1;
+  if (x == 1) return 1;
+  return x * fact(x - 1);
+}
+
+void fact_system(unsigned x) {
+	unsigned k, i, p, factor;
+  int max_factor_i = -1;
+	unsigned result[12] = {0};
+
+  if (x == 0) {
+    printf("0\n");
+    return;
+  }
+
+  for (int i = 1; i < 13; ++i) {
+    if (fact(i) > x) {
+      max_factor_i = i-1;
+      break;
+    }
+    if (fact(i) == x) {
+      max_factor_i = i;
+      break;
+    }
+  }
+  assert(max_factor_i != -1);
+
+  i = max_factor_i;
+  p = 0;
+  for (i = max_factor_i; i > 0; --i) {
+    factor = fact(i);
+    k = x / factor;
+    x = x % factor;
+    result[p] = k;
+    p++;
+  }
+
+	for(int j = 0; j < p; j++) {
+		printf("%u.", result[j]);
+	}
+	printf("\n");
+
+}
+
+
+int main() {
+  int res;
+	unsigned x;
+	res = scanf("%u", &x);
+  if (res != 1) {
+    fprintf(stderr, "Input error");
+    abort();
+  }
+	fact_system(x);
+}
