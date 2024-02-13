c46595dbdde2cc0f794eb0843d9981c8a1b7d277
Tretyakov Leonid <treonid@gmail.com>
2024-02-13 17:01:47 +0300

    Complete sem-2 problem_ft

--- sem-2/problem_ft/main.c
+++ sem-2/problem_ft/main.c
@@ -3,82 +3,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct sieve_t {
-    int n;
-    unsigned char *mod1;
-    unsigned char *mod5;
-};
-
-int is_prime(struct sieve_t *sv, unsigned n);
-
-void set_bit(unsigned char *s, int n) {
-    unsigned byte_shift = n / 8;
-    unsigned bit_n = n % 8;
-    s[byte_shift] = s[byte_shift] | (1u << bit_n);
-}
-
-unsigned char get_bit(unsigned char *s, int n) {
-    unsigned byte_shift = n / 8;
-    unsigned bit_n = n % 8;
-    return (s[byte_shift] >> bit_n) & 1u;
-}
-
-void fill_sieve(struct sieve_t *sv) {
-    int bound = (sv->n * 8) * 6 + 5;
-    set_bit(sv->mod1, 0);
-    for (int i = 2; i * i <= bound; ++i) {
-        if (is_prime(sv, i))
-            for (int j = 2; i * j <= bound; ++j) {
-                if ((i * j - 1) % 6 == 0)
-                    set_bit(sv->mod1, (i * j) / 6);
-                else if ((i * j - 5) % 6 == 0)
-                    set_bit(sv->mod5, (i * j) / 6);
-            }
-    }
-}
-
-int is_prime(struct sieve_t *sv, unsigned n) {
-    int max_size = (sv->n * 8) * 6 + 5;
-    int mod = n % 6;
-    if (n == 2 || n == 3) return 1;
-    if (n > max_size) {
-        printf("Error: out of bounds, %d\n", n);
-        return 0;
-    }
-    if (mod == 1)
-        return (get_bit(sv->mod1, n / 6) == 1u) ? 0 : 1;
-    if (mod == 5)
-        return (get_bit(sv->mod5, n / 6) == 1u) ? 0 : 1;
-    return 0;
-}
-
-int n_count_prime(struct sieve_t *sv, int n) {
-    int max_size = (sv->n * 8) * 6 + 5;
-    int count = 0;
-
-    for (int i = 1; i < max_size; ++i) {
-        if (is_prime(sv, i)) ++count;
-        if (count == n) return i;
-    }
-
-    return -1;
-}
-
-struct sieve_t *sieve_init(long long bound) {
-    struct sieve_t *sv = calloc(1, sizeof(struct sieve_t));
-    sv->n = bound;
-    sv->mod1 = calloc(bound, sizeof(unsigned char));
-    sv->mod5 = calloc(bound, sizeof(unsigned char));
-    fill_sieve(sv);
-    return sv;
-}
-
-void sieve_free(struct sieve_t *sv) {
-    free(sv->mod1);
-    free(sv->mod5);
-    free(sv);
-}
-
 unsigned long long gcd(unsigned long long x, unsigned long long y) {
     unsigned long long q, res;
     do {
@@ -90,15 +14,37 @@ unsigned long long gcd(unsigned long long x, unsigned long long y) {
     return res;
 }
 
+unsigned long long mult_mod(unsigned long long n, unsigned long long k, unsigned long long m) {
+    unsigned long long acc, prod;
+    if (k == 0)
+        return 0;
+    acc = n % m;
+    prod = 0;
+    for (; k > 0;) {
+        if ((k % 2) == 1)
+            prod = (prod + acc) % m;
+        acc = (acc * 2) % m;
+        k = k / 2;
+    }
+    return prod;
+}
+
 unsigned long long pow_mod(unsigned long long n, unsigned long long k, unsigned long long m) {
     unsigned long long mult = n % m;
     unsigned long long prod = 1;
+    unsigned long long m1, m2, m3, m4;
     while (k > 0) {
         if ((k % 2) == 1) {
-            prod = (prod * mult) % m;
+            m1 = prod % m;
+            m2 = mult % m;
+            //            prod = (m1 * m2) % m;
+            prod = mult_mod(m1, m2, m);
             k = k - 1;
         }
-        mult = (mult * mult) % m;
+        m3 = mult % m;
+        m4 = mult % m;
+        // mult = (m3 * m4) % m;
+        mult = mult_mod(m3, m4, m);
         k = k / 2;
     }
     return prod;
@@ -119,15 +65,19 @@ int test_ferma_prime(unsigned long long p) {
 }
 
 int main() {
+    int res;
+    unsigned long long p;
+
+    res = scanf("%lld", &p);
+    if (res != 1) {
+        fprintf(stderr, "Input error\n");
+        abort();
+    }
 
     srand(time(NULL));
-
-    printf("%lld\n", gcd(984, 413958827741)); 
-    printf("%lld\n", pow_mod(984, 413958827741-1, 413958827741)); 
+    printf("%d\n", test_ferma_prime(p));
 
 #ifdef TEST
-    printf("%d\n", test_ferma_prime(413958827741));
-
     assert(test_ferma_prime(100000001) == 0);
     assert(test_ferma_prime(100000007) == 1);
     assert(test_ferma_prime(413958827669) == 0);
