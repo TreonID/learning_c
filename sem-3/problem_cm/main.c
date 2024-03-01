5a9903611a084a87401ab7c578553a17532ea644
Tretyakov Leonid <treonid@gmail.com>
2024-03-01 23:23:11 +0300

    Add test, remove sel sort

--- sem-3/problem_cm/main.c
+++ sem-3/problem_cm/main.c
@@ -18,14 +18,22 @@ void print_arr(int const *arr, unsigned len) {
   printf("\n");
 }
 
+void print_arr_char(char const *arr, unsigned len) {
+  for (int i = 0; i < len; ++i) {
+    printf("%d", arr[i]);
+    if (i != len - 1) printf(" ");
+  }
+  printf("\n");
+}
+
 void xprint_arr(char const *arr, int const *sizes, int nelts) {
   int pos = 0;
 
   for (int i = 0; i < nelts; ++i) {
     if (sizes[i] == 1)
-      printf("(char)%d", arr[pos]);
+      printf("1:%d", arr[pos]);
     if (sizes[i] == 4)
-      printf("(int)%d", *(int *)(arr + pos));
+      printf("4:%d", *(int *)(arr + pos));
     pos += sizes[i];
 
     if (i != nelts - 1) printf(" ");
@@ -42,9 +50,12 @@ int cmp_charint(void *lhs, int lsz, void *rhs, int rsz) {
 typedef int (*xcmp_t)(void *lhs, int lsz, void *rhs, int rsz);
 int ac_pos(int *sizes, int index);
 void copy(void *a, void *b, int count);
-void xmsort_imp(void *mem, int *sizes, xcmp_t cmp, int l, int r);
+void xmsort_imp(void *mem, int *sizes, int nelts, xcmp_t cmp, int l, int r);
 void xmsort(void *mem, int *sizes, int nelts, xcmp_t cmp);
 void xmerge(void *mem, int *sizes, xcmp_t cmp, int l, int m, int r);
+
+int min_abs_pos(void *mem, int *sizes, xcmp_t cmp, int l, int r, char *map);
+void xselsort(void *mem, int *sizes, int nelts, xcmp_t cmp, int l, int r);
 
 int ac_pos(int *sizes, int index) {
   int pos = 0;
@@ -136,28 +147,44 @@ void xmerge(void *mem, int *sizes, xcmp_t cmp, int l, int m, int r) {
   free(tmp_sizes);
 }
 
-void xmsort_imp(void *mem, int *sizes, xcmp_t cmp, int l, int r) {
-  // TODO: Optimize Here!!!
+void xmsort_imp(void *mem, int *sizes, int nelts, xcmp_t cmp, int l, int r) {
   int m = 0;
   if (l >= r) return;
+#if 0
+  if ((r - l) < 40) {
+    xselsort(mem, sizes, nelts, cmp, l, r);
+    return;
+  }
+#endif
   m = (l + r) / 2;
-  xmsort_imp(mem, sizes, cmp, l, m);
-  xmsort_imp(mem, sizes, cmp, m + 1, r);
+  xmsort_imp(mem, sizes, nelts, cmp, l, m);
+  xmsort_imp(mem, sizes, nelts, cmp, m + 1, r);
   xmerge(mem, sizes, cmp, l, m, r);
 }
 
 void xmsort(void *mem, int *sizes, int nelts, xcmp_t cmp) {
-  xmsort_imp(mem, sizes, cmp, 0, nelts - 1);
+  xmsort_imp(mem, sizes, nelts, cmp, 0, nelts - 1);
 }
 
-int min_abs_pos(void *mem, int *sizes, xcmp_t cmp, int l, int r, int *map) {
+#if 0
+int min_abs_pos(void *mem, int *sizes, xcmp_t cmp, int l, int r, char *map) {
   char *cmem = (char *)mem;
 
-  char *p = &cmem[ac_pos(sizes, l)];
+  char *p = NULL;
   int pos = l;
 
   assert(l <= r);
-  if (l == r) return l;
+  if (l == r) {
+    map[l] = 0;
+    return l;
+  }
+
+  for (int i = l; i <= r; ++i)
+    if (map[i] == 1) {
+      p = &cmem[ac_pos(sizes, i)];
+      pos = i;
+      break;
+    }
 
   assert(l < r);
   for (int i = l + 1; i <= r; ++i) {
@@ -166,10 +193,9 @@ int min_abs_pos(void *mem, int *sizes, xcmp_t cmp, int l, int r, int *map) {
     if (cmp(p, sizes[pos], &cmem[ac_pos(sizes, i)], sizes[i]) > 0) {
       pos = i;
       p = &cmem[ac_pos(sizes, i)];
-      map[i] = 0;
     }
   }
-
+  map[pos] = 0;
   return pos;
 }
 
@@ -178,10 +204,9 @@ void xselsort(void *mem, int *sizes, int nelts, xcmp_t cmp, int l, int r) {
   int len_sizes = 1 + r - l;
   char *tmp, tmp_pos = 0;
   int *tmp_sizes, s_pos = 0;
-
   char *cmem = (char *)mem;
 
-  int *map = calloc(nelts, sizeof(char));
+  char *map = calloc(nelts, sizeof(char));
   for (int i = 0; i < nelts; ++i)
     map[i] = 1;
 
@@ -190,49 +215,58 @@ void xselsort(void *mem, int *sizes, int nelts, xcmp_t cmp, int l, int r) {
   tmp = calloc(len_bytes, sizeof(char));
   tmp_sizes = calloc(len_sizes, sizeof(int));
 
-  for (int i = 0; i <= (r - l) ; ++i) {
-    int min_pos = min_abs_pos(mem, sizes, cmp, l + i, r, map);
+  for (int i = 0; i <= (r - l); ++i) {
+    int min_pos = min_abs_pos(mem, sizes, cmp, l, r, map);
     copy(tmp + tmp_pos, &cmem[ac_pos(sizes, min_pos)], sizes[min_pos]);
     tmp_pos += sizes[min_pos];
     tmp_sizes[s_pos] = sizes[min_pos];
     s_pos += 1;
   }
 
-  printf("TMP ARR: ");
-  xprint_arr(tmp, tmp_sizes, len_sizes);
-  printf("L: %d R: %d\n", l, r);
+  copy(cmem + ac_pos(sizes, l), tmp, len_bytes);
+  copy(sizes + l, tmp_sizes, len_sizes * sizeof(int));
 
   free(tmp);
   free(tmp_sizes);
   free(map);
 }
+#endif
 
 int main() {
-  int len = 4, pos = 0;
-  int input[4] = {1004, 3, 5, 2002};
-  int sizes[4] = {4, 1, 1, 4};
+  // Need time report
+  int len = 0;
+  int *sizes;
+  int *input;
   char *arr;
   unsigned byte_size = 0;
+
+  if (scanf("%d", &len) != 1)
+    abort();
+
+  sizes = calloc(len, sizeof(int));
+  input = calloc(len, sizeof(int));
+  for (int i = 0; i < len; ++i) {
+    if (scanf("%d", &input[i]) != 1)
+      abort();
+    sizes[i] = (input[i] < 127) ? 1 : 4;
+  }
 
   for (int i = 0; i < len; ++i)
     byte_size += sizes[i];
 
   arr = calloc(byte_size, sizeof(char));
-
-  for (int i = 0; i < len; ++i) {
-    if (sizes[i] == 4) {
-      *(int *)(arr + pos) = input[i];
-    }
-    if (sizes[i] == 1) {
-      arr[pos] = (char)input[i];
-    }
-    pos += sizes[i];
+  for (int i =0; i < len; ++i) {
+    if (sizes[i] == 1)
+      arr[ac_pos(sizes, i)] = (char)input[i];
+    else
+      *(int *)(arr + ac_pos(sizes, i)) = input[i];
   }
 
   xprint_arr(arr, sizes, len);
-  // xmsort(arr, sizes, len, cmp_charint);
-  xselsort(arr, sizes, len, cmp_charint, 0, 1);
+  xmsort(arr, sizes, len, cmp_charint);
   xprint_arr(arr, sizes, len);
 
   free(arr);
+  free(sizes);
+  free(input);
 }
