dde7821e0d49e3482bfaf6ad343b30614c8a1b1c
Tretyakov Leonid <treonid@gmail.com>
2024-03-12 14:06:50 +0300

    Complete sem-5 problem_sa

---
+++ sem-5/problem_sa/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *strcat_r(char *dest, const char *src, int *bufsz) {
+  int new_len = *bufsz + strlen(src);
+  char *tc = (char *)realloc(dest, new_len);
+  if (tc != NULL) {
+    if (tc != dest)
+      free(dest);
+    tc = strcat(tc, src);
+    *bufsz = new_len;
+    return tc;
+  } else { 
+    fprintf(stderr, "Memory allocation error\n");
+    abort();
+  }
+}
+
+int main() {
+  char *str1 = "Hello";
+  char *str2 = ", world!";
+
+  int bufsz = 6;
+  char *dest = calloc(bufsz, sizeof(char));
+  dest = strcpy(dest, str1);
+  dest = strcat_r(dest, str2, &bufsz);
+
+  printf("[]%s[]\n", dest);
+  free(dest);
+}
