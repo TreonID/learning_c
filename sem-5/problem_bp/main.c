d5c75773a64f340e3a58df79ec8870adbae268eb
Tretyakov Leonid <treonid@gmail.com>
2024-03-14 15:20:53 +0300

    Start sem-5 problem_bp

--- sem-5/problem_bp/main.c
+++ sem-5/problem_bp/main.c
@@ -36,28 +36,44 @@ int calc_change(int n, int m, int *changes, int *V) {
   for (i = 0; i < m; ++i) {
     int CT = calc_change(n - changes[i], m, changes, V);
     if (CT == INT_MAX) continue;
-    V[n] = max(V[n], CT + 1);
+    V[n] = min(V[n], (CT + 1));
   }
+  printf("Assign V[%d]=%d\n", n, V[n]);
   return V[n];
 }
 
+int calc_max(int n, int m, int *changes, int *V) {
+  int i;
+  if (n == 0) return 0;
+  if (n < 0) return INT_MAX;
+  if (V[n] != INT_MAX) return V[n];
+  for (i = 0; i < m; ++i) {
+    int CT = calc_change(n - changes[i], m, changes, V);
+    if (CT == INT_MAX) continue;
+    V[n] = min(V[n], (CT + 1));
+  }
+  printf("Assign V[%d]=%d\n", n, V[n]);
+  return V[n];
+}
+
+
 int main() {
   int m, n, res;
-  int *changes, *V;
+  int *stuffs, *V;
 
   n = read_int();
   m = read_int();
 
-  changes = calloc(m, sizeof(int));
+  stuffs = calloc(m, sizeof(int));
   for (int i = 0; i < m; ++i)
-    changes[i] = read_int();
+    stuffs[i] = read_int();
 
   V = calloc(n + 1, sizeof(int));
   fill_value(V, INT_MAX, n + 1);
 
-  res = calc_change(n, m, changes, V);
+  res = calc_max(n, m, stuffs, V);
   printf("%d\n", res);
 
   free(V);
-  free(changes);
+  free(stuffs);
 }
