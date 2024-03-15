1f6c098a43177b3d34843874644784b9954a9de7
Tretyakov Leonid <treonid@gmail.com>
2024-03-15 13:21:16 +0300

    Update sem-5 problem_bp

--- sem-5/problem_bp/main.c
+++ sem-5/problem_bp/main.c
@@ -27,24 +27,55 @@ int max(int a, int b) {
   return (a > b) ? b : a;
 }
 
+void print_table(int **V, int *stuffs, int s_count, int w) {
+  printf("i\tWi\\W\t");
+  for (int j = 0; j < w + 1; ++j) {
+    printf("%d\t", j);
+  }
+  printf("\n");
+  for (int i = 0; i < s_count + 1; ++i) {
+    for (int j = 0; j < w + 1; ++j) {
+      if (j == 0)
+        printf("%d\t%d\t", i, stuffs[i]);
+
+      printf("%d\t", V[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void build_table(int **V, int *stuffs, int s_count, int w) {
+  for (int i = 0; i < s_count + 1; ++i) {
+    for (int j = 0; j < w + 1; ++j) {
+      if (!i || !j) {
+        V[i][j] = 0;
+        continue;
+      }
+
+      if (j - stuffs[i] >= 0)
+        V[i][j] = 1;
+    }
+  }
+}
+
 int main() {
-  int m, n;
-  int *changes, *V;
-  int res;
+  int w, s_count;
+  int *stuffs, **V;
 
-  n = read_int();
-  m = read_int();
+  w = read_int();
+  s_count = read_int();
 
-  changes = calloc(m, sizeof(int));
-  for (int i = 0; i < m; ++i)
-    changes[i] = read_int();
+  stuffs = calloc(s_count + 1, sizeof(int));
+  for (int i = 1; i < s_count + 1; ++i)
+    stuffs[i] = read_int();
 
-  V = calloc(n + 1, sizeof(int));
-  fill_value(V, INT_MAX, n + 1);
+  V = calloc(s_count + 1, sizeof(int *));
+  for (int i = 0; i < s_count + 1; i++) {
+    V[i] = calloc(w + 1, sizeof(int));
+  }
 
-  res = calc_change(n, m, changes, V);
-  printf("%d\n", res);
+  build_table(V, stuffs, s_count, w);
 
-  free(V);
-  free(changes);
+  print_table(V, stuffs, s_count, w);
+  // TODO: Free mem
 }
